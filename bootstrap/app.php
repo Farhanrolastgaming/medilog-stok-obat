72988d7f3ddb098f06462e9be079b824f2566b34
Farhanrolastgaming <farhanrolastgaming@gmail.com>
2026-07-26 00:40:02 +0700

    Fix Vercel serverless storage permissions

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,3 +19,9 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL']) || getenv('VERCEL')) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+return $app;
